6401d7690258f35d9429f82a6ce10644bfa5d15c
baspi <bartekspitza@hotmail.com>
2022-11-08 16:26:03 +0100

    dot product

--- nn.py
+++ nn.py
@@ -1,8 +1,13 @@
 class Tensor:
   def __init__(self, data):
+    ## Init with shape
+    if isinstance(data, tuple):
+      if len(data) > 1:
+        raise RuntimeError("Init of shapes with dim > 1 not impl!!!!")
+      data = [0] * data[0]
+
+    # Init with actual data
     self.data = data
-    
-    # Compute shape
     self.shape = ()
     curr_dim = data
     while True:
@@ -91,7 +96,37 @@ class Tensor:
         res = tmp.sub(other)
         new_data.append(res.data)
       return Tensor(new_data)
+  
+  def dot(self, other):
+    if not isinstance(other, Tensor):
+      raise TypeError("Not Tensor")
+    
+    if self.dim == 1 and other.dim == 1:
+      if self.shape != other.shape:
+        raise RuntimeError(f'Shape {self.shape} does not match {other.shape}')
 
+      prod = 0
+      for a, b in zip(self.data, other.data):
+        prod += a*b
+      return prod
+
+    if self.dim == 2 and other.dim == 1:
+      new_data = [Tensor(vec).dot(other) for vec in self.data]
+      return Tensor(new_data)
+
+    if self.dim == 1 and other.dim == 2:
+      if self.shape[0] != other.shape[0]:
+        raise RuntimeError(f'Shape {self.shape} does not match {other.shape}')
+      
+      data = Tensor((other.shape[1], ))
+      for i,x in enumerate(self.data):
+        intermediate = Tensor([x*w for w in other.data[i]])
+        data = data.add(intermediate)
+      return data
+    
+    if (self.dim == 2 and other.dim == 2):
+      data = [Tensor(v).dot(other).data for v in self.data]
+      return Tensor(data)
   
   def __repr__(self):
     return f'Tensor(data={self.data.__repr__()})'
--- test.py
+++ test.py
@@ -121,6 +121,62 @@ class Testing(unittest.TestCase):
         c = v.sub(m)
         self.assertEqual([9, 18], c.data[0])
         self.assertEqual([7, 16], c.data[1])
+    
+    # Dot
+    def test_dot_vector_with_vector(self):
+        a = Tensor([1, 2])
+        b = Tensor([10, 20])
+        c = a.dot(b)
+        self.assertEqual(50, c)
+        self.assertEqual(a.dot(b), b.dot(a))
+    
+    def test_dot_vector_with_vector_shapes_mismatch_raises(self):
+        a = Tensor([1])
+        b = Tensor([10, 20])
+        self.assertRaises(RuntimeError, lambda: a.sub(b))
+    
+    def test_dot_2dmatrix_with_vector(self):
+        m = Tensor([[1, 2], [3, 4]])
+        v = Tensor([10, 20])
+        c = m.dot(v)
+        self.assertEqual([50, 110], c.data)
+
+    def test_dot_2dmatrix_with_vector_shapes_mismatch_raises(self):
+        m = Tensor([[1, 2], [3, 4]])
+        v = Tensor([10, 20, 30])
+        self.assertRaises(RuntimeError, lambda: m.dot(v))
+    
+    def test_dot_vector_with_2dmatrix(self):
+        # 3 * 3x2
+        v = Tensor([7, 2, 3])
+        m = Tensor([[1, 2], [3, 4], [5, 6]])
+        c = v.dot(m)
+        self.assertEqual([28, 40], c.data)
+        # 3 * 3x3
+        v = Tensor([1, 2])
+        m = Tensor([[10, 20, 5], [30, 40, 20]])
+        c = v.dot(m)
+        self.assertEqual([70, 100, 45], c.data)
+        # 3 * 3x4
+        v = Tensor([1, 2])
+        m = Tensor([[10, 20, 5, 30], [30, 40, 20, 15]])
+        c = v.dot(m)
+        self.assertEqual([70, 100, 45, 60], c.data)
+    
+    def test_dot_vector_with_2dmatrix_shapes_mismatch_raises(self):
+        v = Tensor([10, 20])
+        m = Tensor([[1, 2], [3, 4], [5,6]])
+        self.assertRaises(RuntimeError, lambda: v.dot(m))
+    
+    def test_dot_2d_with_2d(self):
+        x = Tensor([
+            [7, 2, 3], 
+            [6, 2, 7]
+            ])
+        m = Tensor([[1, 2], [3, 4], [5,6]])
+        c = x.dot(m)
+        self.assertEqual([28, 40], c.data[0])
+        self.assertEqual([47, 62], c.data[1])
 
 
 if __name__ == '__main__':
