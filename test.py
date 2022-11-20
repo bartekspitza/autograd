fix addition gradients: were not accumulating

test.py
@@ -278,8 +278,8 @@ class Testing(unittest.TestCase):
     # ---------------------------------
     # Add
     def test_grad_add_vec_and_vec(self):
-        a = nn.Tensor([2, 2], requires_grad=True)
-        b = nn.Tensor([1, 1], requires_grad=True)
+        a = nn.Tensor([2, -2], requires_grad=True)
+        b = nn.Tensor([1, -1], requires_grad=True)
         c = a+b; c.grad=1
         c.backward()
         self.assertEqual([1, 1], a.grad.data)
@@ -291,23 +291,23 @@ class Testing(unittest.TestCase):
         c = a+b; c.grad=1
         c.backward()
         self.assertEqual([[1,1], [1,1]], a.grad.tolist())
-        self.assertEqual([1,1], b.grad.data)
+        self.assertEqual([2,2], b.grad.data)
     
     def test_grad_add_vec_and_mat(self):
         a = nn.Tensor([[1,1], [2,2]], requires_grad=True)
         b = nn.Tensor([2, 2], requires_grad=True)
         c = b+a; c.grad=1; c.backward()
         self.assertEqual([[1,1], [1,1]], a.grad.tolist())
-        self.assertEqual([1,1], b.grad.data)
+        self.assertEqual([2,2], b.grad.data)
 
     def test_grad_add_mat_and_mat(self):
         a = nn.Tensor([[1, 2], [3, 4]], requires_grad=True)
         b = nn.Tensor([[5, 6], [7, 8]], requires_grad=True)
-        c = b+a; c.grad=1; c.backward()
+        c = a+b; c.grad=1; c.backward()
         self.assertEqual([[1,1], [1,1]], a.grad.tolist())
         self.assertEqual([[1,1], [1,1]], b.grad.tolist())
 
-        a.grad=None; b.grad:None; 
+        a.grad=0; b.grad=0
         c = b+a; c.grad=1; c.backward()
         self.assertEqual([[1,1], [1,1]], a.grad.tolist())
         self.assertEqual([[1,1], [1,1]], b.grad.tolist())