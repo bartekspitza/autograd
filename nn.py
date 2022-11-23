14011966b7f2aa6310d7e3226cb472469ab617aa
baspi <bartekspitza@hotmail.com>
2022-11-23 21:40:58 +0100

    tensor matrix basic ops, but with numpy

--- nn.py
+++ nn.py
@@ -2,9 +2,57 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data, requires_grad=False, backward=None):
-        if not isinstance(data, (list, int, float)):
-            raise TypeError("Cant init with type " + type(data))
+        if isinstance(data, list):
+            data = np.array(data, dtype=float)
+        if isinstance(data, (int, float)):
+            data = np.array([data], dtype=float)
         
         self.data = data
         self.requires_grad = requires_grad
-        self._backward = backward
+        self.backward = backward
+
+    def __add__(self, x):
+        out = Tensor(None, requires_grad=self.requires_grad, backward=self.backward)
+        if isinstance(x, np.ndarray): 
+            out.data = self.data+x
+        else: 
+            out.data = self.data+x.data
+        return out
+    
+    def __mul__(self, x):
+        out = Tensor(None, requires_grad=self.requires_grad, backward=self.backward)
+        if isinstance(x, np.ndarray): 
+            out.data = self.data*x
+        else: 
+            out.data = self.data*x.data
+        return out
+
+    def __sub__(self, x):
+        out = Tensor(None, requires_grad=self.requires_grad, backward=self.backward)
+        if isinstance(x, np.ndarray): 
+            out.data = self.data-x
+        else: 
+            out.data = self.data-x.data
+        return out
+
+    def __truediv__(self, x):
+        out = Tensor(None, requires_grad=self.requires_grad, backward=self.backward)
+        if isinstance(x, np.ndarray): 
+            out.data = self.data/x
+        else: 
+            out.data = self.data/x.data
+        return out
+
+    def __matmul__(self, x):
+        out = Tensor(None, requires_grad=self.requires_grad, backward=self.backward)
+        if isinstance(x, np.ndarray): 
+            out.data = self.data@x
+        else: 
+            out.data = self.data@x.data
+        return out
+    
+    def tolist(self):
+        return self.data.tolist()
+    
+    dim = property(lambda x: x.data.ndim)
+    shape = property(lambda x: x.data.shape)
--- test.py
+++ test.py
@@ -3,7 +3,38 @@ import nn
 import math
 
 class Testing(unittest.TestCase):
-    pass
+
+    def test_init(self):
+        nn.Tensor(3)
+        nn.Tensor([3,3])
+        nn.Tensor([[3,3]])
+    
+    def test_get_dim(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual(1, a.dim)
+
+    def test_get_shape(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual((2,), a.shape)
+
+
+    ## Ops
+    def test_add(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual([6,6], (a+a).tolist())
+    def test_mult(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual([9,9], (a*a).tolist())
+    def test_sub(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual([0,0], (a-a).tolist())
+    def test_div(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual([1,1], (a/a).tolist())
+    def test_matmul(self):
+        a = nn.Tensor([3,3])
+        self.assertEqual(18, (a@a).tolist())
+
     
 if __name__ == '__main__':
     unittest.main()
